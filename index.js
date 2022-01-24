1403584aa4b9f1d77b7e4639b932135a2a6621ce
muturi25 <petermuturi74@gmail.com>
2022-01-24 14:55:01 +0300

    solution

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let results = []
+    for(let i = 0 ; i < names.length; i++) {
+        results.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return results;
+}
+
+function countDown(number) {
+    
+    while(number >= 0) {
+        console.log(number);
+        number--
+    }
+}
+
